0f608f41bf772019006f0ef096b13bc2dbe61fbd
Chloé <ch.guilet@laposte.net>
2023-11-01 22:27:57 +0100

    Création starship service

--- src/app/services/starship.service.ts
+++ src/app/services/starship.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
+import {Starship} from "../models/starship";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarshipService {
+  private url: string = "http://localhost:8080/";
+  private endPoint: string = "starship/";
+  constructor(private httpClient : HttpClient) { }
 
-  constructor() { }
+    public getAllStarship() : Observable<Starship[]> {
+    return this.httpClient.get<Starship[]>(this.url + this.endPoint);
+    }
+
+    public createStarship(starship: Starship): Observable<Starship> {
+        return this.httpClient.post<Starship>(this.url + this.endPoint, starship);
+    }
+
+    public deleteStarship(id: number) : Observable<Starship> {
+    return this.httpClient.delete<Starship>(this.url + this.endPoint + id)
+    }
+
+
 }
